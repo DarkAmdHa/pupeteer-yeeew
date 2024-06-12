c28063f4f26ab718576d0806e704be30988b6482
hamid_tahir <hamidtahir2000@gmail.com>
2024-06-12 07:54:52 +0500

    sda

--- utils/findListingOnGoogle.js
+++ utils/findListingOnGoogle.js
@@ -1,7 +1,7 @@
 import communicateWithOpenAi from "./communicateWithOpenAi.js";
 
 const findListingOnGoogle = async (businessName, platform) => {
-  const prompt = `Use the following Google.com code to look for ${businessName}'s listing on ${platform}. Make sure you return the actual link. There might be ads, or similar looking pages, or reviews or other pages. We only want the listing on the ${platform}, not reviews or other pages related to it.If not found, return an error in json i.e error: "not found"`;
+  const prompt = `Use the following Google.com code to look for ${businessName}'s listing on ${platform}. Make sure you return the actual link if it only partains to the platform ${platform}. There might be ads, or similar looking pages, or even the business' main site, or reviews or other pages but these should all be ignored. We only want the listing of ${businessName} on the ${platform}, not reviews or other pages related to the business.If such a listing is not found in the provided code, return an error in json i.e error: "not found"`;
   const link = `https://www.google.com/search?q=${encodeURIComponent(
     businessName + " " + platform
   )}`;
